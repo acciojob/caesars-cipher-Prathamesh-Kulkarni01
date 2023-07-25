12cdce30c1710308bfedbc8478f0f17cd2ec10f2
Prathamesh Kulkarni <65341679+Prathamesh-Kulkarni01@users.noreply.github.com>
2023-07-25 19:25:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -34,17 +34,17 @@ const lookup = {
 function rot13(encodedStr) {
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
-encodedStr.split().forEach(val=>{
+encodedStr.split('').forEach(val=>{
 	if(val===' ')decodedArr.push(' ')
 	else decodedArr.push(lookup[val])
 })
 	console.log(decodedArr)
-  return decodedArr; //return decodedArr
+  return decodedArr.join(''); //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
 console.log("test",rot13("SERR YBIR? NPPVBWBO"));
-console.log("Hello")
+// console.log("Hello")
 // Do not change this line
 window.rot13 = rot13;
